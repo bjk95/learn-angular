2f1dfea5668aa53dcfc0016d18fea2efc5306611
Bradley Kester <bradleykester@quantexa.com>
2020-03-10 23:52:16 +1100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
+import { ServersComponent } from './servers/servers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'charts', component: ChartsComponent},
+  {path: 'servers', component: ServersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
