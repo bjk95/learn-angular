e9dc7c342a36aea4321914d9a4992cd3cf3a4abd
Bradley Kester <bradleykester@quantexa.com>
2020-03-18 23:16:33 +1100

    added directives to servers component:

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: [
+    './servers.component.css',
+    './servers.component.scss'
+]
 })
 export class ServersComponent implements OnInit {
 
   ID: Number = 666;
   serverName: String = 'The big one';
-  serverStatus: String = 'active';
+  serverStatus: String = 'offlne';
+  newServerName: String = 'Enter name';
+  serverCreated: Boolean = false;
 
   allowNewVM: Boolean = true;
   allowNewDatabase: Boolean = false;
@@ -20,12 +25,24 @@ export class ServersComponent implements OnInit {
     return this.serverStatus;
   }
 
-  constructor() {}
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit() { }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Ready';
+    this.serverCreated = true;
+
+    this.newServerName = 'Done! ' + this.newServerName + ' created';
   }
 
+  onUpdateServerName(event: Event){
+    this.newServerName = (<HTMLInputElement>event.target).value;
+  }
+
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
